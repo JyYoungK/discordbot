68f6651211697c1b1666041609ada5fe64944aba
JyYoungK <junyoung@my.yorku.ca>
2018-08-11 23:41:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,111 +1,100 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
+const botconfig = require("./botconfig.json");
+const fs = require("fs");
 const PREFIX = "askbot: ";
+bot.commands = new Discord.Collection();
+//let cooldown = new Set();
+//let cdseconds = 5;
 
-client.on('ready', () => {
-    console.log('I am ready!');
-    client.user.setActivity("How to become a better bot", {type: "WATCHING"});
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
 });
 
-client.on('message', message => {
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+});
+
+bot.on('message', message => {
     if (message.content === 'ping') {
     	message.channel.send('PONG!');
   	}
 });
 
-var fortunes = [
-  "Yes",
-  "No",
-  "Maybe"
-]
-
-client.on("guildMemberAdd", function(member) {
+bot.on("guildMemberAdd", function(member) {
   member.guild.channels.find("name", "welcome").send(" Welcome to League Discord Channel "+ member.toString()
    + ". Your role has been automatically set to unranked. Type: `askbot: info` to seek help!")
-//  member.addRole(member.guild.roles.find("name", "Unranked"));
 });
 
-client.on("message", function(message){
-
-  if (message.author.equals(client.user)) return;
-
-  if (message.content.startsWith('!Bronze') || message.content.startsWith('!bronze')) {
-  message.channel.send(message.author.toString() + " You have successfully changed your rank to Bronze!");
-  message.member.addRole(message.member.guild.roles.find("name", "Bronze"));﻿}
-
-  if (message.content.startsWith('!Silver') || message.content.startsWith('!silver')) {
-  message.channel.send(message.author.toString() + " You have successfully changed your rank to Silver!");
-  message.member.addRole(message.member.guild.roles.find("name", "Silver"));﻿}
-
-  if (message.content.startsWith('!Gold') || message.content.startsWith('!gold')) {
-  message.channel.send(message.author.toString() + " You have successfully changed your rank to Gold!");
-  message.member.addRole(message.member.guild.roles.find("name", "Gold"));﻿}
-
-  if (message.content.startsWith('!Platinum') || message.content.startsWith('!platinum')) {
-  message.channel.send(message.author.toString() + " You have successfully changed your rank to Platinum!");
-  message.member.addRole(message.member.guild.roles.find("name", "Platinum"));﻿}
-
-  if (message.content.startsWith('!Diamond') || message.content.startsWith('!diamond')) {
-  message.channel.send(message.author.toString() + " You have successfully changed your rank to Diamond!");
-  message.member.addRole(message.member.guild.roles.find("name", "Diamond"));﻿}
-
-  if (message.content.startsWith('!Master') || message.content.startsWith('!master')) {
-  message.channel.send(message.author.toString() + " Sorry, you may not change your rank to this position. Contact admin for more info.");
-  ﻿}
-
-  if (message.content.startsWith('!Challenger') || message.content.startsWith('!challenger')) {
-  message.channel.send(message.author.toString() + " Sorry, you may not change your rank to this position. Contact admin for more info.");
-  ;﻿}
-
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
   if (message.content == "Hello" || message.content == "hi" || message.content == "hello" || message.content == "Hi") {
     message.channel.send("Hi there! I am a bot! :)")};
 
-  if (!message.content.startsWith(PREFIX)) return;
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  //let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
 
-  var args = message.content.substring(PREFIX.length).split(" ");
+  var args2 = message.content.substring(PREFIX.length).split(" ");
 
-  switch (args[0].toLowerCase()) {
-    case "help":
-        message.channel.send("Hi! How can I help you?");
-        break;
-
-    case "roles":
-         message.channel.send('Go to "demand-new-title" and type your Rank position after `!`. For example if you are in Silver, type `!Silver`' );
-         break
-
-    case "ask":
-        if (args[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
-        else message.channel.send("Sorry I can't read that.");
-        break;
-
-    case "info":
-          message.channel.send("Hi! I'm a programmed bot by Chocolate Rose");
-          message.channel.send("Here are useful functions you can ask me");
-          var embed = new Discord.RichEmbed()
-            .addField("askbot: info", "Shows all the functions I can do!", true)
-            .addField("askbot: noticeme", "I will give you attention!", true)
-            .addField("askbot: roles", "I will tell you how to change your role!", true)
-            //.addField("askbot: ", "Test Description", true)
-            //.setColor(0x00FFFF)
-            .setColor(0x00FFFF)
-            .setFooter("This message was pretty cool right?")
-            .setThumbnail(message.author.avatarURL)
-          //  .setDesciption("Hello, this is an awesome embed");
-          message.channel.sendEmbed(embed);
+    switch (args2[0].toLowerCase()) {
+      case "help":
+          message.channel.send("Hi! How can I help you?");
           break;
 
-    /*case "accept":
-            message.member.addRole(message.member.guild.roles.find("name", "Silver"));﻿
-            break;*/
+      case "roles":
+           message.channel.send('Go to "demand-new-title" and type your Rank position after `!`. For example if you are in Silver, type `!Silver`' );
+           break;
 
-    case "noticeme":
-        message.channel.send(message.author.toString() + " Sorry! I missed you!");
-        break;
+      case "ask":
+          if (args2[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+          else message.channel.send("Sorry I can't read that.");
+          break;
 
-    default:
-        message.channel.send("Invalide Command");
-    }
+      case "info":
+            message.channel.send("Hi! I'm a programmed bot by Chocolate Rose");
+            message.channel.send("Here are useful functions you can ask me");
+            var embed = new Discord.RichEmbed()
+              .addField("askbot: info", "Shows all the functions I can do!", true)
+              .addField("askbot: noticeme", "I will give you attention!", true)
+              .addField("askbot: roles", "I will tell you how to change your role!", true)
+              //.addField("askbot: ", "Test Description", true)
+              //.setColor(0x00FFFF)
+              .setColor(0x00FFFF)
+              .setFooter("This message was pretty cool right?")
+              .setThumbnail(message.author.avatarURL)
+            //  .setDesciption("Hello, this is an awesome embed");
+            message.channel.sendEmbed(embed);
+            break;
 
-})
+      case "noticeme":
+          message.channel.send(message.author.toString() + " Sorry! I missed you!");
+          break;
+
+      default:
+          message.channel.send("Invalide Command");
+      }
+
+  setTimeout(() => {
+    cooldown.delete(message.author.id)
+  }, cdseconds * 1000)
+});
 
 client.login(process.env.BOT_TOKEN);
