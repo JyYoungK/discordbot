7d23745b9bcd5a08f4f47c03da21324a98288f8e
JyYoungK <junyoung@my.yorku.ca>
2018-08-11 23:41:21 -0400

    Add files via upload

---
+++ commands/addrole.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  let target = message.member || message.guild.members.get(args[0]);
+  let inputRole = (args.join()).toLowerCase();
+  if(!inputRole) return message.reply("You must enter your rank");
+  let role = message.guild.roles.find(`name`, inputRole);
+  if(!role) return message.reply("Sorry, couldn't find that rank");
+
+  if(target.roles.has(role.id)) return message.reply(`Your title is already ${role.name}`);
+  if(target.roles.size>0){
+    await(target.removeRoles(target.roles.map(role=>role.id)))
+    }
+  await(target.addRole(role))
+  message.channel.send(`Congratulations to ` + target + `! You have successfully changed your rank to ${role.name}!`)
+}
+
+
+
+module.exports.help = {
+  name: "addrole"
+}
