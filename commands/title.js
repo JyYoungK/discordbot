8a1bd0cc79ae188f586d5e4036cbe0a8a54998d4
JyYoungK <junyoung@my.yorku.ca>
2019-01-24 16:58:09 -0500

    Update title.js

--- commands/title.js
+++ commands/title.js
@@ -1,23 +1,25 @@
-const Discord = require("discord.js");
+//This is for S8. This code is now closed.
 
-module.exports.run = async (bot, message, args) => {
+// const Discord = require("discord.js");
 
-  let target = message.member || message.guild.members.get(args[0]);
-  let inputRole = (args.join()).toLowerCase();
-  if(!inputRole) return message.reply("You must enter your rank");
-  let role = message.guild.roles.find(`name`, inputRole);
-  if(!role) return message.reply("Sorry, couldn't find that rank");
+// module.exports.run = async (bot, message, args) => {
 
-  if(target.roles.has(role.id)) return message.reply(`Your title is already ${role.name}`);
-  if(target.roles.size>0){
-    await(target.removeRoles(target.roles.map(role=>role.id)))
-    }
-  await(target.addRole(role))
-  message.channel.send(`Congratulations to ` + target + `! You have successfully changed your rank to ${role.name}!`)
-}
+//   let target = message.member || message.guild.members.get(args[0]);
+//   let inputRole = (args.join()).toLowerCase();
+//   if(!inputRole) return message.reply("You must enter your rank");
+//   let role = message.guild.roles.find(`name`, inputRole);
+//   if(!role) return message.reply("Sorry, couldn't find that rank");
+
+//   if(target.roles.has(role.id)) return message.reply(`Your title is already ${role.name}`);
+//   if(target.roles.size>0){
+//     await(target.removeRoles(target.roles.map(role=>role.id)))
+//     }
+//   await(target.addRole(role))
+//   message.channel.send(`Congratulations to ` + target + `! You have successfully changed your rank to ${role.name}!`)
+// }
 
 
 
-module.exports.help = {
-  name: "title"
-}
+// module.exports.help = {
+//   name: "title"
+// }
